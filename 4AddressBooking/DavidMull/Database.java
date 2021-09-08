063f11687944b10677d69d3d1d5654a272969ffb
david335356 <75817554+david335356@users.noreply.github.com>
2021-09-07 22:44:29 -0500

    Address Book
    Address book with database

---
+++ 4AddressBooking/DavidMull/Database.java
@@ -0,0 +1,11 @@
+
+public class Database {
+    // and so we embark on doing this homework
+    //first we create the database
+    public static final String DATABASE_URL="jdbc:derby://localhost:1527/adressbook";
+    public static final String USERNAME="uta";
+    public static final String PASSWORD="12345";
+    
+    //simple enough now we have completed the   database class
+    //now we create address class
+}
